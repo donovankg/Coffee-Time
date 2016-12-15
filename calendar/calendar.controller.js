318af9808252b1564e76738747d8172c681f04b8
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-12-15 15:04:51 -0600

    change day, week, month

--- calendar/calendar.controller.js
+++ calendar/calendar.controller.js
@@ -1,8 +1,22 @@
 
- angular.module("coffee-time", ['ui.calendar']).controller("calendarCtlr", function() {
+ angular.module("coffee-time", ['ui.calendar']).controller("calendarCtlr", function($scope) {
 
 this.calendarMonth = 'place holder';
-
+/* config object */
+   $scope.uiConfig = {
+     calendar:{
+       height: 450,
+       editable: true,
+       header:{
+         left: 'month basicWeek basicDay agendaWeek agendaDay',
+         center: 'title',
+         right: 'today prev,next'
+       },
+       eventClick: $scope.alertEventOnClick,
+       eventDrop: $scope.alertOnDrop,
+       eventResize: $scope.alertOnResize
+     }
+   };
 
 
 });
