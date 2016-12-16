af70e8527243c3c7654cdacf69f27fd847f8d195
kondapalli tarun <tarunkondapalli@icloud.com>
2016-12-16 08:55:51 -0600

    commit

--- weather/new/next3.js
+++ weather/new/next3.js
@@ -15,13 +15,13 @@ var openWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+ vm.
 $http.get(openWeatherUrl).success(function(data){
 vm.description = data.list[3].weather[0].description;
 // console.log(data.list[0].weather[0].description);
-vm.date= data.list[3].dt;
+vm.date= data.list[6].dt;
 vm.joke = data.city.name;
-vm.speed = (2.237*data.list[0].wind.speed).toFixed(1) + " mph";
-vm.temp = data.list[3].main.temp;
+vm.speed = (2.237*data.list[6].wind.speed).toFixed(1) + " mph";
+vm.temp = data.list[6].main.temp;
 vm.fTemp= (vm.temp*(9/5)-459.67).toFixed(1) + "F " ;
 vm.cTemp= (vm.temp-273).toFixed(1) + "C " ;
-vm.icon = "http://openweathermap.org/img/w/" + data.list[3].weather[0].icon + ".png";
+vm.icon = "http://openweathermap.org/img/w/" + data.list[6].weather[0].icon + ".png";
 
 switch(vm.description){
 case 'mist':{
