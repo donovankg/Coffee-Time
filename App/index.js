4f5a0ea3133a9ef002da3b55323fbdbba5088182
Your Name <you@example.com>
2016-12-16 02:47:50 -0600

    set up views

--- App/index.js
+++ App/index.js
@@ -1,9 +1,38 @@
- angular.module("coffee-time", [])
-
+ angular.module("coffee-time", ['ui.router'])
+ 
  .controller("HelloController", function ($scope) {
  	$scope.helloTo = {};
  	$scope.helloTo.title = "AngularJS";
-	 
 
- });
+ 	console.log("im working");
+ })
 
+ .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+ 	$urlRouterProvider.otherwise('/');
+
+ 	$stateProvider
+ 		
+		.state('root', {
+			url: '',
+			views: {
+				'trending':{
+					url: '/',
+ 					templateUrl: 'App/trendingV.html',
+					controller: 'HelloController'
+				},
+				'transaction': {
+					url: '/',
+ 					templateUrl: 'App/transactionV.html'
+				},
+				'calendar':{
+					url: '/',
+ 					templateUrl: 'App/trendingV.html'
+				}
+			}
+	})
+
+ }]);
+
+ 
+ 
+ 
--- calendar/calendar.controller.js
+++ calendar/calendar.controller.js
@@ -1,23 +1,23 @@
-
-angular.module('coffee-time', ['mwl.calendar','ui.bootstrap']);
-angular
-  .module('coffee-time')
-  .controller('calendarCtlr', function(moment, calendarConfig) {
-
-
-    this.events = [{
-      title: 'No event end date',
-      startsAt: moment().hours(3).minutes(0).toDate(),
-      color: calendarConfig.colorTypes.info
-    }, {
-      title: 'No event end date',
-      startsAt: moment().hours(5).minutes(0).toDate(),
-      color: calendarConfig.colorTypes.warning
-    }];
-
-    this.calendarView = 'month';
-    this.viewDate = new Date();
-    this.addEvent = function(){
-      console.log('add new');
-    }
-  });
+//
+//angular.module('coffee-time', ['mwl.calendar','ui.bootstrap']);
+//angular
+//  .module('coffee-time')
+//  .controller('calendarCtlr', function(moment, calendarConfig) {
+//
+//
+//    this.events = [{
+//      title: 'No event end date',
+//      startsAt: moment().hours(3).minutes(0).toDate(),
+//      color: calendarConfig.colorTypes.info
+//    }, {
+//      title: 'No event end date',
+//      startsAt: moment().hours(5).minutes(0).toDate(),
+//      color: calendarConfig.colorTypes.warning
+//    }];
+//
+//    this.calendarView = 'month';
+//    this.viewDate = new Date();
+//    this.addEvent = function(){
+//      console.log('add new');
+//    }
+//  });
