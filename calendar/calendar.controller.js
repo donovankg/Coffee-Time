66baa4ea17988566d47eaf33efe2ebc8ee5b1388
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-12-16 00:47:56 -0600

    items save in the local storage

--- calendar/calendar.controller.js
+++ calendar/calendar.controller.js
@@ -1,22 +1,54 @@
-
-angular.module('coffee-time', ['mwl.calendar','ui.bootstrap']);
+angular.module('coffee-time', ['mwl.calendar', 'ui.bootstrap']);
 angular
-  .module('coffee-time')
-  .controller('calendarCtlr', function(moment, calendarConfig) {
+    .module('coffee-time')
+    .controller('calendarCtlr', function(moment, calendarConfig) {
+
+        this.event;
+        // this.seed = [{
+        //   title: 'work on this ',
+        //   startsAt: moment().hours(3).minutes(0).toDate(),
+        //   endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+        //   color: calendarConfig.colorTypes.info
+        // }, {
+        //   title: 'No event end date',
+        //   startsAt: moment().hours(5).minutes(0).toDate(),
+        //   endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+        //   color: calendarConfig.colorTypes.warning
+        // }];
+
+        this.loadData = function() {
+            var retrievedData = localStorage.getItem('events');
+            this.events = JSON.parse(retrievedData);
+        }
+        this.toggle = function($event, field, event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            event[field] = !event[field];
+        };
+        this.loadData();
 
 
-    this.events = [{
-      title: 'work on this ',
-      startsAt: moment().hours(3).minutes(0).toDate(),
-      color: calendarConfig.colorTypes.info
-    }, {
-      title: 'No event end date',
-      startsAt: moment().hours(5).minutes(0).toDate(),
-      color: calendarConfig.colorTypes.warning
-    }];
-    this.calendarView = 'month';
-    this.viewDate = new Date();
-    this.addEvent = function(){
-      console.log('add new');
-    }
-  });
+
+        this.calendarView = 'month';
+        this.viewDate = new Date();
+        this.title;
+        this.addEvent = function() {
+
+            this.applyEvent();
+        }
+        this.applyEvent = function() {
+            var newEvent = {
+                title: 'newTitle',
+                startsAt: moment().hours(3).minutes(0).toDate(),
+                endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+
+                color: calendarConfig.colorTypes.info
+            }
+
+            this.events[this.events.length] = newEvent;
+            console.log('- this.events-->', this.events);
+            localStorage.setItem('events', JSON.stringify(this.events));
+
+            this.loadData();
+        }
+    });
